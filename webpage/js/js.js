637c09e924a0e0266f596b80bbb13207ac8002d3
Connor Willoughby <connor.willoughby@callthemarket.com>
2018-07-30 12:35:58 +0100

    1.1.1
    Start here go far!

---
+++ webpage/js/js.js
@@ -0,0 +1,68 @@
+
+var width = window.innerWidth
+var height = window.innerHeight
+var email = ""
+
+var version = "1.1.1"
+var versionName = "Starting Point"
+
+
+$(document).ready(function(){
+
+    console.log(width);
+    console.log(height);    
+    console.log(version+" "+versionName);    
+
+});
+
+
+$("#Submit").click(function(){
+
+    var email = $("#email").val()
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("XMLHTTP","https://europe4.fivecrm.com/123456/index.php?mode=import&hash=<5bfd194daaaf9be8f691a5c91fd93c93>");
+
+    var xmlDoc;
+
+    xmlhttp.onreadystatechange = function() {
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        
+                xmlDoc = xmlhttp.responseXML;
+                console.log(xmlDoc);
+                
+            }
+        };
+    };
+        
+    var xml = '<?xml version="1.0"?><frame name="mainFrame" src="https://europe4.fivecrm.com/123456/index.php"><head></head><importdata><area>007</area><data><notify>connor.willoughby@callthemarket.com</notify><account><field table="TELRCM" field="COMPANY">API Submission</field></account><company><field table="CONT" field="EMAIL">'+(email)+'</field></company></data></area></importdata></frame>';
+    xmlhttp.send(xml);     
+   
+    console.log(email);
+
+});
+
+/*
+<?xml version="1.0"?><importdata><area>007</area><data><notify>connor.willoughby@callthemarket.com</notify><account><field table="TELRCM" field="COMPANY">API Submission</field></account><company><field table="CONT" field="EMAIL">"(email)"</field></company></data></area></importdata>
+*/
+
+
+// var xmlhttp = new XMLHttpRequest();
+// xmlhttp.open("EMAIL","email");
+
+// var xmlDoc;
+
+// xmlhttp.onreadystatechange = function() {
+//     xmlhttp.onreadystatechange = function() {
+//         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+    
+//             xmlDoc = xmlhttp.responseXML;
+            
+//         }
+//     };
+// };
+
+// var xml = '<?xml version="1.0"?><importdata><area>007</area><data><notify>connor.willoughby@callthemarket.com</notify><account><field table="TELRCM" field="COMPANY">API Submission</field></account><company><field table="CONT" field="EMAIL">'+(email)+'</field></company></data></area></importdata>';
+// xmlhttp.send(xml);
+     
