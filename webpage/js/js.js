0cbaacc64c72a1c401d1f77fc44fa906664fb0f5
Connor Willoughby <connor.willoughby@callthemarket.com>
2018-08-06 13:06:46 +0100

    2.1.1
    Lol

--- webpage/js/js.js
+++ webpage/js/js.js
@@ -3,8 +3,8 @@ var width = window.innerWidth
 var height = window.innerHeight
 var email = ""
 
-var version = "1.1.1"
-var versionName = "Starting Point"
+var version = "2.1.1"
+var versionName = "Intresting Name"
 
 
 $(document).ready(function(){
@@ -14,55 +14,3 @@ $(document).ready(function(){
     console.log(version+" "+versionName);    
 
 });
-
-
-$("#submit").click(function(){
-
-    var email = $("#email").val()
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("xml","https://europe7.fivecrm.com/509399/api.php?mode=import&hash=<5bfd194daaaf9be8f691a5c91fd93c93>");
-
-    var xmlDoc;
-
-    xmlhttp.onreadystatechange = function() {
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        
-                xmlDoc = xmlhttp.responseXML;
-                console.log(xmlDoc);
-                
-            }
-        };
-    };
-        
-    var xml = '<?xml version="1.0"?><frame name="mainFrame" src="https://europe7.fivecrm.com/509399/api.php"><head></head><importdata><area>007</area><data><notify>connor.willoughby@callthemarket.com</notify><account><field table="TELRCM" field="COMPANY">API Submission</field></account><company><field table="CONT" field="EMAIL">'+(email)+'</field></company></data></area></importdata></frame>';
-    xmlhttp.send(xml);     
-   
-    console.log(email);
-
-});
-
-/*
-<?xml version="1.0"?><importdata><area>007</area><data><notify>connor.willoughby@callthemarket.com</notify><account><field table="TELRCM" field="COMPANY">API Submission</field></account><company><field table="CONT" field="EMAIL">"(email)"</field></company></data></area></importdata>
-*/
-
-
-// var xmlhttp = new XMLHttpRequest();
-// xmlhttp.open("EMAIL","email");
-
-// var xmlDoc;
-
-// xmlhttp.onreadystatechange = function() {
-//     xmlhttp.onreadystatechange = function() {
-//         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-    
-//             xmlDoc = xmlhttp.responseXML;
-            
-//         }
-//     };
-// };
-
-// var xml = '<?xml version="1.0"?><importdata><area>007</area><data><notify>connor.willoughby@callthemarket.com</notify><account><field table="TELRCM" field="COMPANY">API Submission</field></account><company><field table="CONT" field="EMAIL">'+(email)+'</field></company></data></area></importdata>';
-// xmlhttp.send(xml);
-     
