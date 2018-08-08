9a6304f11899de66028c76e3ce5f064f47c4a200
Connor Willoughby <connor.willoughby@callthemarket.com>
2018-08-08 12:38:21 +0100

    2.3.1
    Apha Testing

--- webpage/js/js.js
+++ webpage/js/js.js
@@ -1,16 +1,14 @@
 
 var width = window.innerWidth
 var height = window.innerHeight
-var email = ""
 
-var version = "2.2.1"
-var versionName = "Intresting Name"
+var version = "2.3.1"
+var versionName = "Alpha"
 
 
 $(document).ready(function(){
 
-    console.log(width);
-    console.log(height);    
+    console.log("Width: " + width + "Height: " + height);
     console.log(version+" "+versionName);    
 
 });
