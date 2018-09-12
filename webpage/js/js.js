b6ee2f12d0ae924b9e96d0c844dd5dc465713516
ConnorWilloughby2 <41489250+ConnorWilloughby2@users.noreply.github.com>
2018-09-12 13:39:15 +0100

    2.4.1
    Minor Changes

--- webpage/js/js.js
+++ webpage/js/js.js
@@ -2,8 +2,8 @@
 var width = window.innerWidth
 var height = window.innerHeight
 
-var version = "2.3.1"
-var versionName = "Alpha"
+var version = "2.4.1"
+var versionName = "Someone is actually intrested in this page now."
 
 
 $(document).ready(function(){
